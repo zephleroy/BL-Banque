54e54af8d0eb485711148efe1b54734b21e6eda7
zephleroy <139692344+zephleroy@users.noreply.github.com>
2023-07-16 14:33:04 +0100

    Update Account.js

--- src/components/Account.js
+++ src/components/Account.js
@@ -6,7 +6,7 @@ const Account = () => {
             <h2 className="sr-only">Accounts</h2>
             <section className="account">
                 <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Checking (x8349)</h3>
+                    <h3 className="account-title">Leykaza Bank Checking (x8349)</h3>
                     <p className="account-amount">$2,082.79</p>
                     <p className="account-amount-description">Available Balance</p>
                 </div>
@@ -16,7 +16,7 @@ const Account = () => {
             </section>
             <section className="account">
                 <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Savings (x6712)</h3>
+                    <h3 className="account-title">Leykaza Bank Savings (x6712)</h3>
                     <p className="account-amount">$10,928.42</p>
                     <p className="account-amount-description">Available Balance</p>
                 </div>
@@ -26,7 +26,7 @@ const Account = () => {
             </section>
             <section className="account">
                 <div className="account-content-wrapper">
-                    <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
+                    <h3 className="account-title">Leykaza Bank Credit Card (x8349)</h3>
                     <p className="account-amount">$184.30</p>
                     <p className="account-amount-description">Current Balance</p>
                 </div>
